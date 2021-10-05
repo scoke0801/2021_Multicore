affe1c529d617b9660d02db2c5d80dc9549f9bb5
LJH <scoke0801@gmail.com>
2021-10-05 14:47:20 +0900

    save

--- Lab2/Lab2/NoMutex.cpp
+++ Lab2/Lab2/NoMutex.cpp
@@ -8,6 +8,9 @@ using namespace chrono;
 
 atomic <int> sum;
 
+volatile int cas_sum;
+volatile int LOCK = 0;
+
 atomic <bool> flags[8];
 atomic <int> labels[8];
 
@@ -16,15 +19,21 @@ mutex myl;
 void worker(int num_threads);
 void worker_mutex(int num_threads);
 void worker_bakery(int num_threads, int th_id);
- 
+void worker_cas(int num_threads);
+
 void bakery_lock(int th_id, int th_count);
 void bakery_unlock(int th_id);
+
+bool CAS(volatile int* addr, int expected, int new_val);
+void CAS_LOCK();
+void CAS_UNLOCK();
 
 int main()
 {
 	// Threads 1,2,4,8
 	for (int i = 1; i <= 8; i *= 2) {
 		sum = 0; 
+		cas_sum = 0;
 		vector <thread> workers;
 		//시작 시간
 		auto start_t = high_resolution_clock::now();
@@ -32,7 +41,8 @@ int main()
 		for (int j = 0; j < i; ++j) {
 			//workers.emplace_back(worker, i);
 			//workers.emplace_back(worker_mutex, i);
-			workers.emplace_back(worker_bakery, i, j);
+			//workers.emplace_back(worker_bakery, i, j);
+			workers.emplace_back(worker_cas, i);
 		}
 		for (auto& th : workers) {
 			th.join();
@@ -44,7 +54,7 @@ int main()
 
 		cout << "number of threads = " << i;
 		cout << "	exec_time = " << duration_cast<milliseconds>(du_t).count();
-		cout << "	sum = " << sum << "\n";
+		cout << "	sum = " << sum << "		cas_sum = " << cas_sum << "\n";
 	}
 } 
 void worker(int num_threads)
@@ -72,6 +82,16 @@ void worker_bakery(int num_threads, int th_id)
 		bakery_lock(th_id, num_threads);
 		sum = sum + 2;
 		bakery_unlock(th_id);
+	}
+}
+
+void worker_cas(int num_threads)
+{
+	const int loop_count = 5000000 / num_threads;
+	for (auto i = 0; i < loop_count; ++i) {
+		CAS_LOCK();
+		cas_sum = cas_sum + 2;
+		CAS_UNLOCK();
 	}
 }
 
@@ -104,4 +124,22 @@ void bakery_lock(int th_id, int th_count)
 void bakery_unlock(int th_id) 
 { 
 	labels[th_id] = 0;
+}
+
+bool CAS(volatile int* addr, int expected, int new_val)
+{
+	return atomic_compare_exchange_strong(reinterpret_cast<volatile atomic_int*>(addr), &expected, new_val);
+}
+
+void CAS_LOCK()
+{
+	while (false == CAS(&LOCK, 0, 1)) {
+
+	} 
+}
+
+
+void CAS_UNLOCK()
+{
+	CAS(&LOCK, 1, 0);
 }
